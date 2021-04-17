d120810ce0aca7f04e75a3c9967a5c0b924ed2f4
italosilva98 <italosilvestre3@icloud.com>
2021-04-17 17:41:12 -0300

    Back-End and Front-End - p1

---
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Evento } from '../_models/Evento';
+import { EventoService } from '../_services/evento.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  eventosFiltrados: any = [];
+  eventos: Evento[];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+  filter: any;
+  modalRef: BsModalRef;
+
+  // _filtroLista: string;
+
+  // get filtroLista(): string{
+  //   return this._filtroLista;
+  // }
+
+  // set filtroLista(value: string){
+  //   this._filtroLista = value
+  //   this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  // }
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+    ) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template)
+  }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  // filtrarEventos(filtrarPor: string): any {
+  //   filtrarPor = filtrarPor.toLocaleLowerCase();
+  //   return this.eventos.filter(
+  //     evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!== -1
+  //   );
+
+  // }
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  getEventos() {
+    this.eventoService.getAllEvento().subscribe(
+      (_eventos: Evento[]) => {
+        this.eventos = _eventos;
+      }, error => {
+        console.log(error);
+      }
+
+    )
+  }
+
+}
---
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.Repository;
+
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ProAgilContext _context { get; }
+
+        public ValuesController(ProAgilContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+
+
+            // return new Evento[] { 
+            //     new Evento(){
+            //         EventoId = 1,
+            //         Tema = "Angular e .NET Core",
+            //         Local = "Belo Horizonte",
+            //         Lote = "1º Lote",
+            //         QtdPessoas = 250,
+            //         DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+            //     },
+            //     new Evento(){
+            //         EventoId = 2,
+            //         Tema = "Angular e .NET Core2",
+            //         Local = "Belo Horizonte",
+            //         Lote = "2º Lote",
+            //         QtdPessoas = 350,
+            //         DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+            //     }
+            // };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+
+
+
+            // return new Evento[] { 
+            //     new Evento(){
+            //         EventoId = 1,
+            //         Tema = "Angular e .NET Core",
+            //         Local = "Belo Horizonte",
+            //         Lote = "1º Lote",
+            //         QtdPessoas = 250,
+            //         DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+            //     },
+            //     new Evento(){
+            //         EventoId = 2,
+            //         Tema = "Angular e .NET Core2",
+            //         Local = "Belo Horizonte",
+            //         Lote = "2º Lote",
+            //         QtdPessoas = 350,
+            //         DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+            //     }
+            // }.FirstOrDefault(x => x.EventoId == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
